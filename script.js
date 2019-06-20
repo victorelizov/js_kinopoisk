708e41437fdb90bd32ba42435e854c8812d46a93
victorelizov <victorelizov@mail.ru>
2019-06-20 14:20:22 +0300

    Added fetch to JS, + style.css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const searchForm = document.querySelector('#search-form');
 
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(event) {
 
@@ -9,31 +10,37 @@ function apiSearch(event) {
     
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=c2388c2b1dae50b4409c8ed48914c075&language=ru&query=' + searchText;
+    movie.innerHTML = "Loading..."
 
-    requestApi('GET', server);
+    fetch(server)
+        .then(function(value){
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json();
+        })
+        .then(function(output) {
+            let inner = '';
+            output.results.forEach(function (item) {
+                let nameItem = item.name || item.title;
+                let dateItem = item.first_air_date || item.release_date;
+                let posterItem = item.poster_path ? (urlPoster + item.poster_path) : 'https://kinomaiak.ru/wp-content/uploads/2018/02/noposter.png';
+                inner += `
+                    <div class="col-12 col-md-4 col-xl-3 item">
+                        <img src="${posterItem}" alt="${nameItem}">
+                        <h5>${nameItem}</h5>
+                        <p><em>(${dateItem})</em></p>
+                    </div>
+                    `;
+             });
+            movie.innerHTML = inner; 
+        })
+        .catch(function(reason){
+            movie.innerHTML = 'Oops, something wrong';
+            console.error('error: ' + reason.status);
+        });
+
 }
 
 
 searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(method, url) {
-    const request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send();
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) return;
-        if (request.status !== 200) {
-            console.log('error: ' + request.status);
-            return;
-        }
-        const output = JSON.parse(request.responseText);
-        let inner = '';
-        output.results.forEach(function (item) {
-            let nameItem = item.name || item.title;
-            let dateItem = item.first_air_date || item.release_date;
-            inner += `<div class="col-6 col-md-4 col-xl-3"><div class="card" style="height: 10rem;"><div class="card-body"><h5 class="card-title">${nameItem}</h5><h6 class="card-subtitle mb-2 text-muted"><em>(${dateItem})</em></h6></div></div></div>`;
-        })
-        movie.innerHTML = inner;
-     
-    });
-}
